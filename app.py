7ff25ebc4a3fb64f7408fe826e26a3c2dec8e0fe
ZachVanDenigg <zachvandenigg@gmail.com>
2024-10-13 17:51:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-# from flask import Flask, render_template, request
+from flask import Flask, render_template, request
 
-# # Create a flask app
-# app = Flask(
-#   __name__,
-#   template_folder='templates',
-#   static_folder='static'
-# )
+# Create a flask app
+app = Flask(
+  __name__,
+  template_folder='templates',
+  static_folder='static'
+)
 
-# @app.get('/')
-# def index():
-#   return render_template('index.html')
+@app.get('/')
+def index():
+  return render_template('index.html')
 
 # @app.get('/hello')
 # def hello():
